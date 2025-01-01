16ffa072d2a26600c9ddc6547a3eef124030ac06
prasannajaga <prasannajaga9@gmail.com>
2025-01-01 13:53:24 +0530

    betaseries implemented

--- client/src/components/authService/baseHttp.ts
+++ client/src/components/authService/baseHttp.ts
@@ -2,8 +2,12 @@ import axios from "axios";
 
 // const KEY = 'dddf94a2';
 // const URL = `http://www.omdbapi.com/?apikey=${KEY}&page=1`;
-export const MOVIEURL = 'https://whatson-api.onrender.com/?limit=100';
+// export const MOVIEURL = 'https://whatson-api.onrender.com/?limit=100';
+export const MOVIEURL = 'https://api.betaseries.com/movies/';
 
 export const baseHttp =  axios.create({
     baseURL : MOVIEURL, 
+    headers : {
+        "X-BetaSeries-Key" : "f7fa6a75ea5a"
+    }
 }) 
--- client/src/components/authService/movieService.ts
+++ client/src/components/authService/movieService.ts
@@ -1,17 +1,59 @@
-import { baseHttp, MOVIEURL } from "./baseHttp"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { baseHttp } from "./baseHttp"
 
+const advancedSearch  = (limit :string = "100") => `https://api.betaseries.com/search/movies?locale=en&limit=${limit}&`;
 
-
-export const getMovies = async () => {
-    const response  = await baseHttp.get('');    
-    return response;
+export const getAllMovies = async (genres : string) => {
+ const URL = advancedSearch("100") + `genres=${genres === undefined ? "" :genres}`
+ const response  = await baseHttp.get(URL);  
+ return response;
 }
 
-export const getMoviesById = async (id : number) => {
-    const data = MOVIEURL.split("?");
-    const movieURL = data[0] + `movie/${id}`;
-    const response  = await baseHttp.get(movieURL);    
-    return response;
+export const getSimiliarMovies = async (genres : string) => {
+ const URL = advancedSearch("20") + `${genres === undefined ? "" : 'genres=' + genres}`
+ const response  = await baseHttp.get(URL);  
+ return response;
+}
+
+export const getCastByMovies = async (movieId : string) => {
+ const URL = `characters?id=${movieId}&locale=en`
+ const response  = await baseHttp.get(URL);  
+ return response;
+}
+
+export const getMovies = async () => {
+ const URL = 'random?locale=en&nb=20'
+ const response  = await baseHttp.get(URL);  
+ return response;
+}
+
+export const searchMovies = async (genres : string, searchTerm : string) => {
+ const URL = advancedSearch + `genres=${genres === undefined ? "" : genres}&text=${searchTerm}`
+ const response  = await baseHttp.get(URL);  
+ return response;
+}
+
+export const getUpcomingMovies = async () => {
+ const URL = advancedSearch("10") + "releases=2022,2024,2025"
+ const response  = await baseHttp.get(URL);  
+ return response;
+}
+ 
+export const getMoviesById = async (id : number) => { 
+ const movieURL = `/movie?id=${id}&locale=en`
+ const response  = await baseHttp.get(movieURL); 
+ return response;
+}
+
+export const getGenreCollections = async () => { 
+ const movieURL = `/genres?locale=en`
+ const response  = await baseHttp.get(movieURL); 
+ return response;
+}
+
+export const saveMovies = async (datas : Record<string , any>) => {
+ const response  = await baseHttp.post("http://localhost:8080/movie/save" , datas);  
+ return response;
 }
 
 
